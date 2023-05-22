7c67309380def97faea224bb9e10e8fbecfbcbb5
Eduardo Toste <duciochetti@gmail.com>
2023-05-21 23:19:15 -0300

    Criando e utilizando Exceptions

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,35 +1,12 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import br.com.alura.bytebank.print.printMsg
-import java.lang.ClassCastException
-import java.lang.NumberFormatException
 
 
 fun main() {
     println("início main")
 
-    val entrada: String = "1.6"
-
-    val valorRecebido: Double? = try{
-        entrada.toDouble()
-    } catch (e: NumberFormatException){
-        println("Problema na conversao!")
-        e.printStackTrace()
-        null
-    }
-
-    val valorComTaxa: Double? = if(valorRecebido != null){
-        valorRecebido + 0.1
-    } else {
-        null
-    }
-
-    if(valorComTaxa != null){
-        println("Valor recebido: $valorComTaxa")
-    } else {
-        println("Valor invalido!")
-    }
 
     funcao1()
     println("fim main")
@@ -39,8 +16,8 @@ fun funcao1(){
     println("início funcao1")
     try{
         funcao2()
-    } catch (e: ClassCastException){
-        printMsg("ClassCastException foi captada!")
+    } catch (e: SaldoInsuficienteException){
+        printMsg("SaldoInsuficienteException foi captada!")
         e.printStackTrace()
     }
     println("fim funcao1")
@@ -48,10 +25,9 @@ fun funcao1(){
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
+    for (i in 1..5) {
         println(i)
-        val endereco = Any()
-        endereco as Endereco
+        throw SaldoInsuficienteException()
     }
     println("fim funcao2")
 }
